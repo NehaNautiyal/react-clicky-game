13f4dcc2402af511025e31a578e080e4d23ba0de
Neha Nautiyal <nehahaha@gmail.com>
2019-06-22 13:14:49 -0400

    Updates

---
+++ precache-manifest.eba202d87dc4f9fcfc85e2733ea29202.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa7fc93eedd4acc8eddc31d06860c53",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "ca2578348f304470056b",
+    "url": "/react-clicky-game/static/css/main.62ebb093.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ca2578348f304470056b",
+    "url": "/react-clicky-game/static/js/main.4c2a95a4.chunk.js"
+  },
+  {
+    "revision": "a3b74089770802ae2f4d",
+    "url": "/react-clicky-game/static/js/runtime~main.a57faf00.js"
+  }
+]);
